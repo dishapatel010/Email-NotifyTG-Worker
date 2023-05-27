fe6c990275ed425423420e1986bf86ce07e7998b
Lylia <71930916+dishapatel010@users.noreply.github.com>
2023-05-27 12:11:13 +0530

    VIEW HTML - D1 (#1)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ notification to Telegram by @dishapatel010
 */
 
 const PostalMime = require('postal-mime');
+
+// Define a list of email addresses and their corresponding chatIds and forward email ids
+      const emailList = [
+        { to: "example@gmail.com", chatId: 5071059420, fMailid: "example@example.com", toForward: false }, //only sends notification as its false
+        { to: "example1@example.com", chatId: 5071059420, fMailid: "example3@example.com", toForward: true }, //sends & forward
+        { to: "example2@example.com", chatId: 5071059420, fMailid: "example4@example.com", toForward: false },
+      ];
+
 async function streamToArrayBuffer(stream, streamSize) {
   let result = new Uint8Array(streamSize);
   let bytesRead = 0;
@@ -25,11 +33,32 @@ function emailToChatIdMap(emailList) {
   });
   return map;
 }
+
 export default {
+  async fetch(req, env, context) {
+    try {
+      const url = new URL(req.url)
+      const id = url.pathname.split('/')[1] // get the id from the URL
+      const data = await env.DB.prepare(`SELECT html FROM CFWTGW WHERE id = ?`).bind(id).raw(); // prepare SQL statement with bound parameter
+      if (data.length === 0) {
+        throw new Error('No data found');
+      }
+      const htmlContent = data[0];
+      const r = new Response(htmlContent, {
+        headers: { 'Content-Type': 'text/html' },
+      });
+      return r;
+    } catch (e) {
+      console.error(e);
+      return new Response(`An error occurred: ${e.message}`, { status: 500 });
+    }
+  },
+  
   async email(message, env, ctx) {
     // Define the Telegram API URL
-    const tgToken = env.TOKEN //env VAR
+    const tgToken = env.TOKEN;
     const telegramUrl = `https://api.telegram.org/bot${tgToken}/sendMessage`;
+    const cfurl = env.CFURL;
     try {
       // Extract the email data
       const from = message.headers.get("from");
@@ -59,13 +88,6 @@ export default {
       } else {
         notificationMessage += `📎 ${attachmentCount} attachments`;
       }
-      // Define a list of email addresses and their corresponding chatIds and forward email ids
-      const emailList = [
-        { to: "example@gmail.com", chatId: 5071059420, fMailid: "example@example.com", toForward: false }, //only sends notification as its false
-        { to: "example1@example.com", chatId: 5071059420, fMailid: "example3@example.com", toForward: true }, //sends & forward
-        { to: "example2@example.com", chatId: 5071059420, fMailid: "example4@example.com", toForward: false },
-      ];
-
       // Map the email addresses to their corresponding chatIds and forward email ids
       const emailToChatId = emailToChatIdMap(emailList);
       // Find the chatId and fMailid for the current email based on its "to" address
@@ -74,6 +96,23 @@ export default {
         console.log(`No matching chatId or fMailid found for email to ${to}`);
         return;
       }
+      const c = new Date().getTime();
+     if (parsedEmail.html !== undefined) {
+  const dhtml = parsedEmail.html;
+  try {
+    if (c && dhtml && cfurl) { // Check that both values are defined
+      await env.DB.prepare('INSERT INTO CFWTGW (id, html) VALUES (?, ?)').bind(c, dhtml).run();
+    } else {
+      console.log('Error: One or more values are undefined');
+    }
+  } catch (e) {
+    console.log({
+      message: e.message,
+      cause: e.cause ? e.cause.message : null
+    });
+    // Handle the error as needed
+  }
+}
       // Paste the raw message and html version into Spacebin
       const spacebinUrl = "https://spaceb.in/api/v1/documents/";
       const spacebinData = {
@@ -97,9 +136,17 @@ export default {
       const spacebinJson = await spacebinResponse.json();
       const sbinUrl = `https://spaceb.in/${spacebinJson.payload.id}`;
       // Add the Spacebin URL to the notification message as an inline button
-      const buttonText = "View Spacebin";
-      const button = { text: buttonText, url: sbinUrl };
-      const replyMarkup = { inline_keyboard: [[button]] };
+      const buttonText1 = "View Spacebin";
+      const button1 = { text: buttonText1, url: sbinUrl };
+      let replyMarkup;
+      if (parsedEmail.html !== undefined) {
+            const durl = cfurl + c;
+            const buttonText2 = "View Html";
+            const button2 = { text: buttonText2, url: durl };
+            replyMarkup = { inline_keyboard: [[button1, button2]] }
+      } else {
+            replyMarkup = { inline_keyboard: [[button1]] };
+      }
       // Send the notification to Telegram with the inline button
       await fetch(telegramUrl, {
         method: "POST",
