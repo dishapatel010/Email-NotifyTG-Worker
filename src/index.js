decda2acfd9c5473f0bc7a253669858b766a8064
Lylia <71930916+dishapatel010@users.noreply.github.com>
2023-07-01 19:45:34 +0530

    add: View TEXT

--- src/index.js
+++ src/index.js
@@ -122,9 +122,184 @@ export default {
         date: date,
         text: parsedEmail.text,
         html: parsedEmail.html,
+        count: attachmentCount,
       };
       
       const mypaste = `${spacebinData.title}\nFrom: ${spacebinData.from}\nTo: ${spacebinData.to}\nDate: ${spacebinData.date}\nText Version:\n${spacebinData.text}\nHtml Version:\n${spacebinData.html}`;
+      const zc = new Date().getTime(); //new time for mytext
+      const mytext = `
+  <!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Email</title>
+    <style>
+      /* Common CSS styles */
+      body {
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+      .email-container {
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+        margin: 20px auto;
+        max-width: 600px;
+        padding: 20px;
+      }
+      h2 {
+        font-size: 24px;
+        line-height: 28px;
+        margin-bottom: 10px;
+      }
+      p {
+        font-size: 16px;
+        line-height: 20px;
+        margin: 0 0 10px;
+      }
+      pre {
+        background-color: #eee;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        font-family: monospace;
+        font-size: 14px;
+        line-height: 18px;
+        padding: 10px;
+        white-space: pre-wrap;
+        word-wrap: break-word;
+        margin: 0 0 20px;
+      }
+      
+      /* Light theme CSS */
+      .light-theme {
+        background-color: #f6f6f6;
+        color: #333;
+      }
+      .light-theme .email-container {
+        background-color: #fff;
+      }
+      
+      /* Dark theme CSS */
+      .dark-theme {
+        background-color: #333;
+        color: #fff;
+      }
+      .dark-theme .email-container {
+        background-color: #222;
+        border: 1px solid #555;
+      }
+      .dark-theme h2 {
+        color: #fff;
+      }
+      .dark-theme p {
+        color: #ccc;
+      }
+      .dark-theme pre {
+        background-color: #444;
+        border: 1px solid #333;
+      }
+      
+      /* Toggle button styles */
+      .toggle {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        background: transparent;
+        border: none;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+      }
+      .toggle span {
+        font-size: 24px;
+      }
+    </style>
+  </head>
+  <body onload="setTheme()">
+    <div class="email-container">
+      <h2>${spacebinData.title}</h2>
+      <p>From: ${spacebinData.from}</p>
+      <p>To: ${spacebinData.to}</p>
+      <p>Date: ${spacebinData.date}</p>
+      <p>Text Version:</p>
+      <pre>${spacebinData.text}</pre>
+      <p>Attachment Count: ${spacebinData.count}</p>
+    </div>
+
+    <button class="toggle" onclick="toggleTheme()">
+      <span id="sun-icon">&#x1f31e;</span> 
+      <span id="moon-icon">&#x1f319;</span>
+    </button>
+
+    <script>
+      function setTheme() {
+        const body = document.body;
+        const theme = getCookie('theme');
+        if (theme === 'dark') {
+          body.classList.add('dark-theme');
+        }
+        setIcon(theme);
+      }
+
+      function toggleTheme() {
+        const body = document.body;
+        body.classList.toggle('dark-theme');
+        const theme = body.classList.contains('dark-theme') ? 'dark' : 'light';
+        setCookie('theme', theme, 365);
+        setIcon(theme);
+      }
+
+      function setIcon(theme) {
+        const sunIcon = document.getElementById('sun-icon');
+        const moonIcon = document.getElementById('moon-icon');
+        if (theme === 'dark') {
+          sunIcon.style.display = 'inline-block';
+          moonIcon.style.display = 'none';
+        } else {
+          sunIcon.style.display = 'none';
+          moonIcon.style.display = 'inline-block';
+        }
+      }
+
+      function setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = 'expires=' + date.toUTCString();
+        document.cookie = name + '=' + value + '; ' + expires + '; path=/';
+      }
+
+      function getCookie(name) {
+        const cookieName = name + '=';
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+          let cookie = cookies[i];
+          while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+          }
+          if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+          }
+        }
+        return '';
+      }
+    </script>
+  </body>
+</html>
+`;
+
+      try {
+  if (zc && mytext) {
+    // Perform something
+    await env.DB.prepare('INSERT INTO CFWTGW (id, html) VALUES (?, ?)').bind(zc, mytext).run();
+  } else {
+    // Pass
+    console.log('Error: One or more values are undefined mytext');
+  }
+} catch (error) {
+  console.log('An error occurred:', error);
+}
+
       const spacebinResponse = await fetch(spacebinUrl, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -138,14 +313,17 @@ export default {
       // Add the Spacebin URL to the notification message as an inline button
       const buttonText1 = "View Spacebin";
       const button1 = { text: buttonText1, url: sbinUrl };
+      const zburl = cfurl + zc;
+      const buttonText3 = "View TEXT";
+      const button3 = { text: buttonText3, url: zburl };
       let replyMarkup;
       if (parsedEmail.html !== undefined) {
             const durl = cfurl + c;
-            const buttonText2 = "View Html";
+            const buttonText2 = "View HTML";
             const button2 = { text: buttonText2, url: durl };
-            replyMarkup = { inline_keyboard: [[button1, button2]] }
+            replyMarkup = { inline_keyboard: [[button1, button2], [button3]] }
       } else {
-            replyMarkup = { inline_keyboard: [[button1]] };
+            replyMarkup = { inline_keyboard: [[button1, button3]] };
       }
       // Send the notification to Telegram with the inline button
       await fetch(telegramUrl, {
